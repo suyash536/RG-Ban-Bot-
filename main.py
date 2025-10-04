a3a0d5c9ba8a0a9a37ffa4167d76d21c3607e8ab
suyash536 <suyashpatidar009@gmail.com>
2025-10-04 18:14:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, ConversationHandler
 
 
-TOKEN = "7117024517:AAGqEroxmqrysuckqAswLBYitMmUB9haVbs"  
+TOKEN = "8465506921:AAFe97FKh-GnAnpPR7OC15TgvIz6az19viI"  
 
 fake = Faker()
 USERNAME, = range(1)
